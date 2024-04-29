612f82a64315a0628dba08309f9ea1fa0e791983
Mahammud Hassan Limon <mahammudhassanlimon@gmail.com>
2024-04-29 13:33:54 +0600

    first commit

---
+++ main/laora.js
@@ -0,0 +1,8 @@
+function hidesideber() {
+  const sideber = document.querySelector(".sideber");
+  sideber.style.display = "flex";
+}
+function showber() {
+  const sideber = document.querySelector(".sideber");
+  sideber.style.display = "none";
+}
